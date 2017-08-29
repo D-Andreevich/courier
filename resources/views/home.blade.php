5f42cde44fa1147a2e0631bbabc34989bc0db71f
Shakhman <ooox1991@gmail.com>
2017-08-29 13:03:01 +0300

    modifed home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,18 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+            <div>
+                <!-- Nav tabs -->
+                <ul class="nav nav-tabs" role="tablist">
+                    <li class="active"><a href="#courier" aria-controls="courier" role="tab" data-toggle="tab">Найти курьера</a></li>
+                    <li><a href="#order" aria-controls="order" role="tab" data-toggle="tab">Найти заказ</a></li>
+                </ul>
+
+                <!-- Tab panes -->
+                <div class="tab-content">
+                    <div class="tab-pane active" id="courier">@include('find-courier')</div>
+                    <div class="tab-pane" id="order">@include('find-order')</div>
+                </div>
 
         </div>
     </div>
