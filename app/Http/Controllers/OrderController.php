7a342c06284a1e68ac1fa6d62daecdfcf4e9553b
D-Andreevich <daniilshvorak@gmail.com>
2017-09-08 13:49:58 +0300

    save point

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Order;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Route;
+use Grimzy\LaravelMysqlSpatial\Types\Point;
+
 
 class OrderController extends Controller
 {
@@ -36,20 +38,34 @@ class OrderController extends Controller
 			'address_a' => $data['address_a'],
 			'address_b' => $data['address_b'],
 			'price' => $data['price'],
-            'coordinate_a' => $data['address_a'],
-            'coordinate_b' => $data['address_b']
+			'coordinate_a' => $data['coordinate_a'],
+			'coordinate_b' => $data['coordinate_b'],
 		]);
+
+        /*$pointA= explode( ', ', $data['coordinate_a']);
+        $pointB= explode( ', ', $data['coordinate_b']);
+        $address= new Order();
+        $address->user_id =  $data['user_id'];
+        $address->quantity =  $data['quantity'];
+        $address->width =  $data['width'];
+        $address->height =  $data['height'];
+        $address->depth =  $data['depth'];
+        $address->weight =  $data['weight'];
+        $address->time_of_receipt =  $data['time_of_receipt'];
+        $address->description =  $data['description'];
+        $address->name_receiver =  $data['name_receiver'];
+        $address->phone_receiver =  $data['phone_receiver'];
+        $address->email_receiver =  $data['email_receiver'];
+        $address->address_a =  $data['address_a'];
+        $address->address_b =  $data['address_b'];
+        $address->price =  $data['price'];
+
+        $address->coordinate_a = new Point($pointA[0],$pointA[1]);
+        $address->coordinate_b = new Point($pointB[0],$pointB[1]);
+        $address->save();*/
 		
 		$request->session()->flash('previous-route', Route::current()->getName());
 		
 		return redirect('/');
 	}
-
-    public function store()
-    {
-        $bushWalk = Bushwalk::create(Request::except('startPoint'));
-        $myPoint = Request::input('startPoint');
-        $bushWalk->startPoint = \DB::raw($myPoint);
-        $bushWalk->save();
-    }
 }
