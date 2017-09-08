604c112bcb5a0ea85b3fec3a9b335856adaf0cc7
D-Andreevich <daniilshvorak@gmail.com>
2017-09-08 15:13:56 +0300

    save point to mysql

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -26,25 +26,6 @@ class OrderController extends Controller
 		$pointA= explode( ', ',$data['coordinate_a']);
 		$pointB= explode( ', ', $data['coordinate_b']);
 
-//		Order::create([
-//			'user_id' => $data['user_id'],
-//			'quantity' => $data['quantity'],
-//			'width' => $data['width'],
-//			'height' => $data['height'],
-//			'depth' => $data['depth'],
-//			'weight' => $data['weight'],
-//			'time_of_receipt' => $data['time_of_receipt'],
-//			'description' => $data['description'],
-//			'name_receiver' => $data['name_receiver'],
-//			'phone_receiver' => $data['phone_receiver'],
-//			'email_receiver' => $data['email_receiver'],
-//			'address_a' => $data['address_a'],
-//			'address_b' => $data['address_b'],
-//			'price' => $data['price'],
-//			'coordinate_a' => new Point($pointA[0],$pointA[1]),
-//			'coordinate_b' => new Point($pointB[0],$pointB[1])
-//		]);
-		
         $address= new Order();
         $address->user_id =  $data['user_id'];
         $address->quantity =  $data['quantity'];
