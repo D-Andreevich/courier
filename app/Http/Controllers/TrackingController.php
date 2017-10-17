a20ea1c3a9dceb7f5aede68727dadf89769feed2
Shakhman <ooox1991@gmail.com>
2017-10-17 19:41:54 +0300

    some fixes

--- app/Http/Controllers/TrackingController.php
+++ app/Http/Controllers/TrackingController.php
@@ -17,7 +17,7 @@ class TrackingController extends Controller
     	if ($order->status === 'taken') {
 		    return view('track/tracking');
 	    } else {
-    		return route('home');
+    		return redirect('/');
 	    }
     }
 
