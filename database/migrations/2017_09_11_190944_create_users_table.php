358e3fc05e929758758ed4cab1dc0137b2bcfd7f
d-andreevich <qwertys7654321@gmail.com>
2018-03-15 16:25:15 +0200

    add laradock

--- database/migrations/2017_09_11_190944_create_users_table.php
+++ database/migrations/2017_09_11_190944_create_users_table.php
@@ -20,7 +20,14 @@ class CreateUsersTable extends Migration
 			$table->string('email', 50)->unique();
 			$table->string('password');
 			$table->string('avatar');
-			$table->boolean('activated')->default(false);
+            $table->decimal('rating', '2', '1')->default(0);
+            $table->integer('total_rating')->unsigned()->default(0);
+            $table->geometry('current_position')->nullable();
+            $table->integer('total_rates')->unsigned()->default(0);
+            $table->string('avatar')->default('/uploads/avatars/default.jpg')->change();
+            $table->decimal('social_id', '30','0');
+            $table->boolean('is_tracking')->default(0);
+            $table->boolean('activated')->default(false);
 			$table->rememberToken();
 			$table->timestamps();
 		});
