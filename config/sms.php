08696d9183a2757c68f0ff5bb3ff778345e9a7e5
d-andreevich <qwertys7654321@gmail.com>
2018-03-21 13:21:05 +0200

    add send sms

--- config/sms.php
+++ config/sms.php
@@ -3,7 +3,7 @@
 return [
     'status' => env('SMS_STATUS', 0),
     'class' => env('SMS_CLASS', ''),
-    'caption' => env('SMS_CAPTION', 'Kurier+'),
+    'caption' => env('SMS_CAPTION', 'Kurier'),
     'apiKey' => env('SMS_API_KEY', null),
     'login' => env('SMS_LOGIN', null),
     'password' => env('SMS_PASSWORD', null),
